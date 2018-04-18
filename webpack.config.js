371e4824d49902b4188727750ceb4aeff66cd082
Letasian <18770913233@163.com>
2018-04-18 14:09:06 +0800

    webpack基础知识

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,39 @@
 module.exports = {
-    entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
-    output: {
-      path: __dirname + "/public",//打包后的文件存放的地方
-      filename: "bundle.js"//打包后输出文件的文件名
-    },
-    mode: 'production'
+  devtool: 'eval-source-map',
+  entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
+  output: {
+    path: __dirname + "/public", //打包后的文件存放的地方
+    filename: "bundle.js" //打包后输出文件的文件名
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true,
+    inline: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader",
+        },
+        exclude: /node_modules/
+      },
+      {
+        test:/\.css$/,
+        use:[
+          {
+            loader: "style-loader"
+          }, {
+              loader: "css-loader",
+              options:{
+                modules:true,
+                localIdentName:'[name]__[local]--[hash:base64:5]'
+              }
+          }
+        ]
+      }
+    ]
   }
+}
